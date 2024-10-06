b4f49e64f5f7957a3d1ed217af323b3f5d44913a
owang <hilow0090@gmail.com>
2024-10-05 20:49:09 -0700

    changed base url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = process.env.REACT_APP_BASE_URL;
-const BASE_URL = "https://jobly-backend-tujy.onrender.com";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+// const BASE_URL = "https://jobly-backend-tujy.onrender.com";
 
 /** API Class.
  *
